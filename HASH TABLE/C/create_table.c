d21b74d88760a0dfe9af46384e81c04211bac419
sundayirvine-code <amugumbiirvine@gmail.com>
2023-01-08 23:51:58 +0300

    create documentation for create_table.c

--- HASH TABLE/C/create_table.c
+++ HASH TABLE/C/create_table.c
@@ -1,5 +1,12 @@
 #include "hash.h"
 
+/**
+ * hash_table_create - creates a  hash table
+ * @size: size of the array
+ *
+ * Return: Pointer to the newly created hash table, otherwise NULL
+ */
+
 // Create a new hash table
 hash_table *create_hash_table(int size) {
     hash_table *ht = malloc(sizeof(hash_table));
